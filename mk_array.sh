f38f82004c1b37af67232fece768fede459cc6eb
digi-deity <git@laurens.xyz>
2026-06-19 10:38:31 +0200

    Fix setup scripts

--- mk_array.sh
+++ mk_array.sh
@@ -2,12 +2,12 @@ echo ">>> [mk_array] Creating NonRAID array with disk assignments"
 nmdctl create --force P:/dev/loop0p1:virtdisk-001 Q:/dev/loop1p1:virtdisk-002 1:/dev/loop2p1:virtdisk-003 2:/dev/loop3p1:virtdisk-004
 
 echo ">>> [mk_array] Starting the NonRAID array"
-sudo nmdctl start
+echo 'y' | nmdctl start
 sleep 1
 
-echo ">>> [mk_array] Running array check (no correct)"
-sudo nmdctl -u check
-sleep 3
+echo ">>> [mk_array] Running array check"
+echo 'y' |nmdctl check
+sleep 1
 
 echo ">>> [mk_array] Mounting array disks"
 nmdctl mount
